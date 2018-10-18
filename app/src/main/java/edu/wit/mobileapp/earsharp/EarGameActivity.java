18a05e5268b98d318b2131588908045f394c8525
Scherflinge <scherflinge@wit.edu>
2018-10-18 09:00:00 -0400

    New Screens Created

---
+++ app/src/main/java/edu/wit/mobileapp/earsharp/EarGameActivity.java
@@ -0,0 +1,13 @@
+package edu.wit.mobileapp.earsharp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EarGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ear_game);
+    }
+}
---
+++ app/src/main/java/edu/wit/mobileapp/earsharp/GameSetupActivity.java
@@ -0,0 +1,14 @@
+
+package edu.wit.mobileapp.earsharp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_setup);
+    }
+}
