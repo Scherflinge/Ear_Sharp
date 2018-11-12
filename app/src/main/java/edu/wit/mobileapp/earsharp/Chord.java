67691a621c55d379fb538b6b76021e9818ddbcc0
amandadomanowski <domanowskia@wit.edu>
2018-11-12 01:02:31 -0500

    started Chord and MidiTranslator

--- app/src/main/java/edu/wit/mobileapp/earsharp/Chord.java
+++ app/src/main/java/edu/wit/mobileapp/earsharp/Chord.java
@@ -1,4 +1,14 @@
 package edu.wit.mobileapp.earsharp;
 
 public class Chord {
+    //chord root
+
+    //chord extension
+
+    /**
+     * constructor
+     */
+    Chord () {
+
+    }
 }
---
+++ app/src/main/java/edu/wit/mobileapp/earsharp/MidiTranslator.java
@@ -0,0 +1,7 @@
+package edu.wit.mobileapp.earsharp;
+
+public class MidiTranslator {
+    //play method
+
+    //stop method
+}
