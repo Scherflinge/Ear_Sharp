9a5b15f69dacd2d5c36fb6d50bde700392e4560c
austins-wit <austins@wit.edu>
2018-11-15 12:19:05 -0500

    PlayNoteListener methods implemented in EarGameActivity. The notes will highlight and unhighlight as instructed Added Spinner with a submit guess button for selecting an interval to guess and sending it to the game class (temporary UI for testing) Debug button added for testing startNewRound

--- app/src/main/java/edu/wit/mobileapp/earsharp/activities/EarGameActivity.java
+++ app/src/main/java/edu/wit/mobileapp/earsharp/activities/EarGameActivity.java
@@ -1,9 +1,16 @@
 package edu.wit.mobileapp.earsharp.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,12 +22,21 @@ import edu.wit.mobileapp.earsharp.game.EarGame;
 import edu.wit.mobileapp.earsharp.game.PlayNoteListener;
 import edu.wit.mobileapp.earsharp.music.Chord;
 import edu.wit.mobileapp.earsharp.music.Extension;
+import edu.wit.mobileapp.earsharp.music.Interval;
 import edu.wit.mobileapp.earsharp.music.Note_Enum;
 
-public class EarGameActivity extends AppCompatActivity implements PlayNoteListener {
+public class EarGameActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,
+        PlayNoteListener {
 
-    EarGame game;
-    List<Chord> cmaj = Arrays.asList(
+    // Controls
+    private Button btnReplay;
+    private Spinner spnInterval;
+    private List<ImageView> imageViews;
+
+    // Game
+    private EarGame game;
+    private Interval selectedInterval;
+    private List<Chord> testLesson = Arrays.asList(
             new Chord(Note_Enum.A1, Extension.None),
             new Chord(Note_Enum.B1, Extension.None),
             new Chord(Note_Enum.C1, Extension.None),
@@ -36,6 +52,8 @@ public class EarGameActivity extends AppCompatActivity implements PlayNoteListen
             new Chord(Note_Enum.F2, Extension.None),
             new Chord(Note_Enum.G2, Extension.None)
     );
+    private List<Interval> testIntervals = Arrays.asList(Interval.I, Interval.II, Interval.III,
+            Interval.IV, Interval.V, Interval.VI, Interval.VII);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,45 +65,84 @@ public class EarGameActivity extends AppCompatActivity implements PlayNoteListen
 
         setTitle(getString(R.string.ear_game));
 
-        game = new EarGame(this, cmaj, Difficulty.Easy);
+        game = new EarGame(this, testLesson, Difficulty.Easy);
+//        game.startNewRound();
 
-        Button btnEarGame = (Button)findViewById(R.id.button_results);
-        btnEarGame.setOnClickListener((view) -> {
-            Intent intent = new Intent();
-            intent.setClass(EarGameActivity.this, ResultsScreenActivity.class);
+        imageViews = new ArrayList<>();
+        imageViews.add((ImageView)findViewById(R.id.image_light_1));
+        imageViews.add((ImageView)findViewById(R.id.image_light_2));
+        imageViews.add((ImageView)findViewById(R.id.image_light_3));
+        imageViews.add((ImageView)findViewById(R.id.image_light_4));
+        imageViews.add((ImageView)findViewById(R.id.image_light_5));
+        imageViews.add((ImageView)findViewById(R.id.image_light_6));
+        imageViews.add((ImageView)findViewById(R.id.image_light_7));
+        imageViews.add((ImageView)findViewById(R.id.image_light_8));
 
-            Bundle outBundle = new Bundle();
-            // TODO Add Bundle Data Here
-
-            intent.putExtras(outBundle);
-            startActivity(intent);
+        btnReplay = (Button)findViewById(R.id.button_replay);
+        btnReplay.setOnClickListener((view) -> {
+            btnReplay.setEnabled(false);
+            game.playChord();
         });
 
-        Button btnBack = (Button)findViewById(R.id.back_button);
-        btnBack.setOnClickListener((view) -> {
-            Intent intent = new Intent();
-            intent.setClass(EarGameActivity.this, GameSetupActivity.class);
+        Button btnTestStart = (Button)findViewById(R.id.button_teststart);
+        btnTestStart.setOnClickListener((view) -> {
+            game.startNewRound();
+        });
 
-            Bundle outBundle = new Bundle();
-            // TODO Add Bundle Data Here
+        spnInterval = (Spinner)findViewById(R.id.spinner_intervals);
+        ArrayAdapter<Interval> spinnerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, testIntervals);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnInterval.setAdapter(spinnerAdapter);
+        spnInterval.setOnItemSelectedListener(this);
 
-            intent.putExtras(outBundle);
-            startActivity(intent);
+        Button btnGuess = (Button)findViewById(R.id.button_guess);
+        btnGuess.setOnClickListener((view) -> {
+            game.checkInterval(selectedInterval);
+
+//            Intent intent = new Intent();
+//            intent.setClass(EarGameActivity.this, ResultsScreenActivity.class);
+//
+//            Bundle outBundle = new Bundle();
+//            // TODO Add Bundle Data Here
+//
+//            intent.putExtras(outBundle);
+//            startActivity(intent);
         });
     }
 
     @Override
-    public void highlightNotes(List<Integer> intervalList) {
+    protected void onResume() {
+        super.onResume();
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        selectedInterval = (Interval)parent.getItemAtPosition(pos);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        selectedInterval = null;
+    }
+
+    @Override
+    public void highlightNotes(List<Integer> intervals) {
+        for (Integer interval : intervals) {
+            imageViews.get(interval).setColorFilter(getResources().getColor(R.color.colorHighlightNote));
+        }
     }
 
     @Override
     public void unhighlightNotes() {
+        for (ImageView imageView : imageViews) {
+            imageView.clearColorFilter();
+        }
 
     }
 
     @Override
     public void donePlayingNotes() {
-
+        btnReplay.setEnabled(true);
     }
 }
