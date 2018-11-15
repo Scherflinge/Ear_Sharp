ea7d69b0e47ae3482f7949f4099e56ae531ae1b8
amandadomanowski <domanowskia@wit.edu>
2018-11-15 00:26:18 -0500

    having issues with version/API on my studio end. not fully sure if this works but it's something at least

--- app/src/main/java/edu/wit/mobileapp/earsharp/music/MidiTranslator.java
+++ app/src/main/java/edu/wit/mobileapp/earsharp/music/MidiTranslator.java
@@ -1,21 +1,59 @@
 package edu.wit.mobileapp.earsharp.music;
 
+import java.util.List;
+
+import javax.sound.midi.*;
+
 public class MidiTranslator implements MidiTranslator_Interface {
+    static MidiChannel[] mc;
+    private int velocity = 300;
+
     @Override
     public void playChord(Chord chord) {
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        synth.open();
+        mc = synth.getChannels();
+        Instrument[] inst = synth.getDefaultSoundbank().getInstruments();
+        synth.loadInstrument(instr[0]);
 
+        List<Extension> extensions = chord.getExtensions();
+
+        int root = chord.getRoot().name.indexOf(chord.getRoot().toString()) + 20;
+
+        mc[0].noteOn(root, velocity);
+        mc[0].noteOn(root + Integer.parseInt(extensions.get(1).toString()), velocity);
+        mc[0].noteOn(root + Integer.parseInt(extensions.get(2).toString()), velocity);
+
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e) {}
+
+        mc[0].noteOff(root);
+        mc[0].noteOff(root + Integer.parseInt(extensions.get(1).toString()));
+        mc[0].noteOff(root + Integer.parseInt(extensions.get(2).toString()));
     }
 
     @Override
     public void playNote(Note_Enum note) {
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        synth.open();
+        mc = synth.getChannels();
+        Instrument[] inst = synth.getDefaultSoundbank().getInstruments();
+        synth.loadInstrument(instr[0]);
 
+        mc[0].noteOn(note.name.indexOf(note.name) + 20, velocity);
+
+        try {
+            Thread.sleep(1000);
+        }catch (InterruptedException e) {}
+
+        mc[0].noteOff(note.name.indexOf(note.name) + 20);
     }
 
     @Override
     public void stopPlaying() {
 
     }
-    //play method
 
-    //stop method
+
 }
